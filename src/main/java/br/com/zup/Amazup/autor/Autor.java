1e9a5d1cae93a6442ddff12f6fbeda43c1102cc6
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-12-13 14:40:24 -0300

    Criar Entidad Autor

---
+++ src/main/java/br/com/zup/Amazup/autor/Autor.java
@@ -0,0 +1,37 @@
+package br.com.zup.Amazup.autor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    public Autor() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
